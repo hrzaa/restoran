1c4c3810bd8d71d576b0a175a2053550d6e8ec42
hrzaa <harezayoan@gmail.com>
2023-07-27 01:54:17 +0700

    change sidbar title

--- view/admin/header.php
+++ view/admin/header.php
@@ -17,7 +17,7 @@
         <!-- Sidebar -->
         <div class="bg shadow" id="sidebar-wrapper">
             <div class="sidebar-heading text-center py-4 primary-text fs-4 fw-bold text-uppercase border-bottom"><i
-                    class="fas fa-user-secret me-2"></i>Codersbite</div>
+                    class="fas fa-user-secret me-2"></i>Resto MNG</div>
             <div class="list-group list-group-flush my-3">
                 <a href="home-admin.php" class="list-group-item list-group-item-action bg-transparent second-text active"><i
                     class="fas fa-tachometer-alt me-2"></i>Dashboard</a>
